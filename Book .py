717c36482372e74e4a3432a6db5e2624bb89d2dc
riyakrishna8c-349 <riyakrishna8c@gmail.com>
2025-11-23 22:29:50 +0530

    Create Book .py

---
+++ Book .py
@@ -0,0 +1,10 @@
+# books.py
+
+books = [
+    {"Book": "The Prodigy", "Author": "Marie", "Book_ID": "30AAB", "Genre": "Autobiography", "Status": "Available"},
+    {"Book": "His Duchess", "Author": "shaer", "Book_ID": "03AAC", "Genre": "Romance", "Status": "Available"},
+    {"Book": "Care", "Author": "Marie", "Book_ID": "36AAF", "Genre": "Mystery", "Status": "Available"},
+    {"Book": "Mend", "Author": "ezra", "Book_ID": "27AAG", "Genre": "Thriller", "Status": "Available"},
+    {"Book": "Knox", "Author": "Fera", "Book_ID": "29AAA", "Genre": "Horror", "Status": "Available"},
+    {"Book": "Etheral", "Author": "Era rexon", "Book_ID": "29AAM", "Genre": "Fiction", "Status": "Available"},]
+
